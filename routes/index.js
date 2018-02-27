462fe2b5154c1ca1fa448ac81b330dd7e6a8acbe
harmanjeetkaur <98harmanjeetkaur@gmail.com>
2018-02-26 23:10:54 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: '',
-                        message: ''});
+                        message: '  '});
 });
 
 //GET: /About
 router.get('/about', (req, res, next)=> {
     //load the about view
     res.render('about', {
-        title: 'About me',
+        title: '',
         message:''
     });
 });
